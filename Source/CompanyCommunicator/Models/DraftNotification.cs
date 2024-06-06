6fbfbe91ebe234ccc82ad12038a6752d7f3a8d32
Sai Venkat <98143492+gsv022@users.noreply.github.com>
2024-06-06 18:32:46 +0530

    Increased  MaxSelectedTeamNum to 40

--- Source/CompanyCommunicator/Models/DraftNotification.cs
+++ Source/CompanyCommunicator/Models/DraftNotification.cs
@@ -17,7 +17,7 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Models
     /// </summary>
     public class DraftNotification : BaseNotification
     {
-        private static readonly int MaxSelectedTeamNum = 20;
+        private static readonly int MaxSelectedTeamNum = 40;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="DraftNotification"/> class.
